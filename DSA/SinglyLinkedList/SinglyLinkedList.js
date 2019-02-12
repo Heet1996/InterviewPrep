56a0071a03e734cb3a11a91ba20901c901011b02
Heet1996 <heet1885@gmail.com>
2019-02-12 22:46:45 +0530

    Done with get solution

--- DSA/SinglyLinkedList/SinglyLinkedList.js
+++ DSA/SinglyLinkedList/SinglyLinkedList.js
@@ -34,28 +34,61 @@ class SinglyLinkedList
     {   
 
         let temp=this.head;
+        if(temp==null) return undefined;
         if(temp.next==null)
         {
             this.head=null;
             this.tail=null;
             this.length=0;
-            return this;
+            return temp.value;
         }
         while(temp.next!=this.tail)
             temp=temp.next;
+        let value=temp.next.value;
         temp.next=null;    
         this.tail=temp;
         this.length-=1;
-        return this;
+        return value;
 
     }
-
+    shift()
+    {
+        if(this.head===null) return undefined;
+        let temp=this.head.next;
+        let val=this.head;
+        if(temp===null) 
+        {   
+            this.head=this.tail=null;
+            this.length=0;
+            return val;
+        }
+        
+        this.head=temp;
+        this.length-=1;
+        return val;
+    }
+    unshift(val)
+    {
+        let node=new Node(val);
+        node.next=this.head;
+        if(!this.head) this.head=this.tail=node;
+        else  this.head=node;
+        this.length+=1;
+        return this;
+    }
+    get(pos)
+    {   let temp=this.head;
+        while(pos && temp.next)
+        {
+            temp=temp.next;
+            pos--;
+        }
+        if(pos) return null;
+        return temp.value;
+    }
 }
 let list=new SinglyLinkedList();
 list.push(3);
-list.push(4);
-list.push(5);
 
-console.log(list.pop());
-console.log(list.pop());
-console.log(list.pop());
+
+console.log(list.get(2));
