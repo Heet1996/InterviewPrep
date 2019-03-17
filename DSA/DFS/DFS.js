a03a4d8358758a60aa762a33d41806c41d64ab8c
Heet1996 <heet1885@gmail.com>
2019-03-17 10:18:56 +0530

    Done with DFS

--- DSA/DFS/DFS.js
+++ DSA/DFS/DFS.js
@@ -1,14 +1,23 @@
 let BinaryTree=require('../BinaryTree/BinaryTree');
 
 class DFS extends BinaryTree
-{
+{   
+    preorder(fromNode=this.root,values=[])
+    {    
+        
+        if(fromNode==null) return values;
+        values.push(fromNode.val)
+        if(fromNode.left) { this.preorder(fromNode.left,values) }
+        if(fromNode.right) { this.preorder(fromNode.right,values)}
+        return values;
+    }
     inorder(fromNode=this.root,values=[])
-    {    values=[...values];   
-         if((fromNode.left && fromNode.right)) return;
-         values.push(fromNode.val);
-         if(fromNode.left) this.inorder(fromNode.left,values)
-         if(fromNode.right) this.inorder(fromNode.right,values)
-
+    {
+        if(fromNode==null) return values;
+        if(fromNode.left) this.inorder(fromNode.left,values)
+        values.push(fromNode.val);
+        if(fromNode.right) this.inorder(fromNode.right,values);
+        return values;
     }   
 }
 let dfs=new DFS();
@@ -17,4 +26,5 @@ dfs.insert(dfs.root,4);
 dfs.insert(dfs.root,5);
 dfs.insert(dfs.root,6);
 
-console.log(dfs.inorder(dfs.root));
+// console.log(dfs.preorder());
+console.log(dfs.inorder());
