524c42a6aa53580e1397c89098f7fdf718ef86f2
Heet1996 <heet1885@gmail.com>
2019-10-14 23:57:43 +0530

    added graph functions

---
+++ DSA/Graph/Graph.js
@@ -0,0 +1,51 @@
+class Graph
+{
+    constructor()
+    {
+        this.adjacencyList={};
+    }
+    addVertex(v){
+        if(this.adjacencyList[v]) return;
+        this.adjacencyList[v]=[];
+    }
+    addEdge(v1,v2){
+        if(v1==v2) return;
+        if(!v1||!v2) return;
+        if(!this.adjacencyList[v1]) this.adjacencyList[v1]=[];
+        if(!this.adjacencyList[v2]) this.adjacencyList[v2]=[];
+        if(this.adjacencyList[v1].indexOf(v2)>-1 || this.adjacencyList[v2].indexOf(v1)>-1)  return;
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+        
+    }
+    removeEdge(v1,v2)
+    {
+           
+        if(v1==v2) return;
+        if(!v1|| !v2) return;
+        if(!this.adjacencyList[v1] || !this.adjacencyList[v2]) return;
+        if(this.adjacencyList[v1].indexOf(v2)==-1 || this.adjacencyList[v2].indexOf(v1)==-1) return;
+        
+        this.adjacencyList[v1].splice(this.adjacencyList[v1].indexOf(v2),1);
+        this.adjacencyList[v2].splice(this.adjacencyList[v2].indexOf(v1),1);
+
+    }
+    removeVertex(v)
+    {
+        let arr=[...this.adjacencyList[v]];
+        for(let i=0;i<arr.length;i++)
+            this.removeEdge(arr[i],v);
+        this.adjacencyList[v]
+
+    }
+}
+let g=new Graph();
+g.addVertex("Paris");
+g.addVertex("Tokyo");
+g.addVertex("Mumbai");
+g.addVertex("Chennai");
+g.addEdge("Paris","Mumbai");
+g.addEdge("Paris","Chennai");
+g.addEdge("Paris","Hawaii");
+g.removeVertex("Paris")
+console.log(g.adjacencyList);
--- DSA/HashMap/HashTable.js
+++ DSA/HashMap/HashTable.js
@@ -1,12 +1,11 @@
 class HashTable
 {
-    constructor(size=53)
+    constructor(size=3)
     {
         this.keys=new Array(size);
     }
     get(key)
-    {
-        let index=this._hash(key);
+    {   let index=this._hash(key);
         if(index) 
         {
             let arr=this.keys[index];
@@ -14,13 +13,20 @@ class HashTable
             arr.forEach(el => {
                  if(Object.keys(el)[0]==key) {o=el}
             });
-            return o;
+            return Object.values(o);
         }
 
     }
     set(key,value)
     {   
-        let index=this._hash(key);
+        let index=0;
+        try{
+        index=this._hash(key);
+        }
+        catch(e)
+        {   
+            throw e;
+        }
         this.keys[index].push({[key]:value});
         return this.keys;
     }
@@ -28,19 +34,30 @@ class HashTable
     {
      const WEIRD_PRIME =31;
      let total=0;
-     
      for(let i=0;i<key.length;i++)
         {   
             total=(total*WEIRD_PRIME+key.charCodeAt(i))%this.keys.length;
         }
      if(!this.keys[total])   
-     this.keys[total]=new Array();   
+     this.keys[total]=new Array();
+     for (const el in this.keys[total]) {
+         
+            if(Object.keys(this.keys[total][el])[0]==key)
+                throw new Error(` ${key} is repeated more than once`);
+     }   
+     
      return total;    
     }
 
 }
 let hash=new HashTable();
-hash.set('pink','#ddsfs');
-hash.set('pink','#ddsss');
-hash.set('pik','#desss');
-console.log(hash.get('pik'));
+try {
+    hash.set('pink','#ddsfs');
+    hash.set('blue','#ddsss');
+    hash.set('pink','#fesgs');
+    hash.set('orange','#edslq');
+    hash.set('yellow','#kjdsf');    
+} catch (error) {
+    console.log(error.message);
+}
+
