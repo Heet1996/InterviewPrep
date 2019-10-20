fee0a26d20c0098e25600790373f9b01ea8ee802
Heet1996 <heet1885@gmail.com>
2019-10-20 12:19:55 +0530

    added recursive solution

--- DSA/Graph/Graph.js
+++ DSA/Graph/Graph.js
@@ -38,14 +38,42 @@ class Graph
         delete this.adjacencyList[v]
 
     }
+    dfs(v,visited,result)
+    {
+        if(!this.adjacencyList[v]) return;
+        if(!visited[v]) 
+        {   visited[v]=true;
+            result.push(v);
+        }
+        else return;
+        for (const i of this.adjacencyList[v]) {
+            this.dfs(i,visited,result);
+        }
+    } 
+    dfs_recursive(v)
+    {
+        if(!this.adjacencyList[v]) return null;
+        let visited={};
+        let result=[];
+        this.dfs(v,visited,result);
+        return result;
+    }
 }
 let g=new Graph();
-g.addVertex("Paris");
-g.addVertex("Tokyo");
-g.addVertex("Mumbai");
-g.addVertex("Chennai");
-g.addEdge("Paris","Mumbai");
-g.addEdge("Paris","Chennai");
-g.addEdge("Paris","Hawaii");
-g.removeVertex("Paris")
-console.log(g.adjacencyList);
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A","B");
+g.addEdge("A","C");
+g.addEdge("B","D");
+g.addEdge("C","E");
+g.addEdge("D","E");
+g.addEdge("D","F");
+g.addEdge("E","F");
+
+console.log(g.dfs_recursive("A"));
+
